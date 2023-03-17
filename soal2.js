c429e0225120ac5bc6b624964d5d8cb999b79ff5
Latifano <ikromilatifano76@gmail.com>
2023-03-17 10:12:55 +0700

    tambah comment di soal2.js

--- soal2.js
+++ soal2.js
@@ -1,15 +1,17 @@
 exports.checkTypeNumber = (givenNumber) => {
-        givenNumber = parseInt(givenNumber)
-        if (givenNumber % 2 === 0){
-            return `Bilangan ` + givenNumber + ` adalah GENAP`;
-        }else if(givenNumber % 2 === 1){
-            return `Bilangan ` + givenNumber + ` adalah GANJIL`;
-        }else{
-            return `Error : Invalid data type`;
-        }
+  givenNumber = parseInt(givenNumber);
+  if (givenNumber % 2 === 0) {
+    return `Bilangan ` + givenNumber + ` adalah GENAP`;
+  } else if (givenNumber % 2 === 1) {
+    return `Bilangan ` + givenNumber + ` adalah GANJIL`;
+  } else {
+    return `Error : Invalid data type`;
+  }
 };
 /* String "" masih membaca sbg integer
-*/
+ */
+
+// console.log di comment karena ketika soal2.js di export ke soal3.js dengan adanya fungsi yang sedang dipanggil, maka checkTypeNumber di file soal3.js jadi error. Oleh karena itu 6 baris dibawah untuk merunning checkTypeNumber() di comment dulu
 
 // console.log(checkTypeNumber(10));
 // console.log(checkTypeNumber(3));
